8deeee19c867b476f0f0695b32ad4364dbf6bff6
Shree Kumar <shree.shree@gmail.com>
2025-08-25 05:34:23 +0530

    Mac(arm64) container in ZIP package

--- build-container-package.sh
+++ build-container-package.sh
@@ -1,6 +1,7 @@
 rm -rf soib-container
 mkdir soib-container
 mkdir soib-container/x86_64
+mkdir soib-container/arm64
 mkdir soib-container/output
 cp output/config.R soib-container/output
 cp run_container.sh soib-container/
@@ -9,6 +10,7 @@ cp install-x86_64-container.sh soib-container/
 cp install-arm64-container.sh soib-container/
 
 podman build --layers --squash-all -t soib .
-podman save -o soib-container/x86_64/soib.tar localhost/soib:latest
+# container in appropriate arch dir
+podman save -o soib-container/`uname -m`/soib.tar localhost/soib:latest
 rm soib-container.zip
 zip -r soib-container.zip soib-container
