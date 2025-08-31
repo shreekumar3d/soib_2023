a76f4b9052f275f3a81a4c5ce59a235ad9ac1e32
Shree Kumar <shree.shree@gmail.com>
2025-08-31 10:42:24 +0530

    Correct file paths in packaging

--- build-container-package.sh
+++ build-container-package.sh
@@ -12,13 +12,13 @@ cp config/localhost/config.R soib-container/config/localhost
 cp run_container_package.sh soib-container/run_container.sh
 cp 00_data/analyses_metadata.RData soib-container/data/00_data
 cp 01_analyses_full/specieslists.RData soib-container/data/01_analyses_full
-cp 00_data/species_names.RData soib-container/data/00_data
-cp 00_data/timegroups.RData soib-container/data/00_data
 cp 00_data/current_soib_migyears.RData soib-container/data/00_data
 
 # The full data, metadata
 cp 01_analyses_full/dataforanalyses.RData-data_opt soib-container/data/01_analyses_full
 cp 01_analyses_full/dataforanalyses.RData-metadata soib-container/data/01_analyses_full
+cp 01_analyses_full/species_names.RData soib-container/data/01_analyses_full
+cp 01_analyses_full/timegroups.RData soib-container/data/01_analyses_full_data
 # All random groupids
 cp 01_analyses_full/rgids-*.RData soib-container/data/01_analyses_full/
 # Stats to guide scheduling
