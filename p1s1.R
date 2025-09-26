5ddacdd12235b4db06a1890f1b06a0056a8c7c6d
Shree Kumar <shree.shree@gmail.com>
2025-09-26 06:08:03 +0530

    Update to latest sensitive species file

--- p1s1.R
+++ p1s1.R
@@ -39,5 +39,5 @@ source("00_scripts/01_create_metadata.R")
 
 tic("Reading and cleaning raw data")
 readcleanrawdata(rawpath = "00_data/ebd_IN_unv_smp_relAug-2025.txt",
-                 sensitivepath = "00_data/ebd_sensitive_relJun-2025_IN.txt")
+                 sensitivepath = "00_data/ebd_sensitive_relAug-2025_IN.txt")
 toc() # 42 min
